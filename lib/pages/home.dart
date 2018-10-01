bf5c028a3f45f6eccf414f20b43136f977ef9589
Jordi Vidal <jordividal1997@gmail.com>
2018-10-01 18:21:56 +0200

    Drawer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import '../product_manager.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
-    Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('BuyList'),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,//quitar el icono de drawer
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {},
+            )
+          ],
         ),
-        body: ProductManager(),
-      );
-    }
+      ), //drawwer left side - endrawer right side / Multiple items use scrollable
+      appBar: AppBar(
+        title: Text('BuyList'),
+      ),
+      body: ProductManager(),
+    );
+  }
 }
-
-
