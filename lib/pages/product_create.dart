31e3adc9173f29af04ad87d1778f6f5b7d6e86ae
Jordi Vidal <jordividal1997@gmail.com>
2018-10-02 20:25:32 +0200

    TextField

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,17 +1,55 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatPage extends StatelessWidget {
+class ProductCreatPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatPage> {
+  String titleValue;
+  String descriptionValue;
+  double priceValue = 0.0;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: (){//sheet bottom of the page *Modal* put additional info
-          showModalBottomSheet(context: context, builder: (BuildContext context){
-            return Center(child: Text('This is a modal'),);//mini page
-          } );
-        },
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          //autofocus que cuando se inicialize la page salga el teclado
+          //onCganged trigger que cada vez que cambia
+          TextField(
+            decoration: InputDecoration(
+                /*icon: Icon(Icons.link)*/ labelText: 'Product Title'),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            maxLines: 3,
+            decoration: InputDecoration(labelText: 'Product Description'),
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Price'),
+            keyboardType: TextInputType.number,
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+          ),
+          //Text(titleValue),
+        ],
       ),
     );
   }
--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -4,6 +4,20 @@ class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(child: Text('asa'));
+    return Center(
+      child: RaisedButton(
+        child: Text('Save'),
+        onPressed: () {
+          //sheet bottom of the page *Modal* put additional info
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Center(
+                  child: Text('This is a modal'),
+                ); //mini page
+              });
+        },
+      ),
+    );
   }
 }
